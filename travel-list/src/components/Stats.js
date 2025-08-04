760c2cee62064b5262cba7afe21b7caac6ce3138
qhuy135520 <huyltqse135@gmail.com>
2025-08-04 21:46:46 +0700

    2025/08/04: finished thinking in react - state management

---
+++ travel-list/src/components/Stats.js
@@ -0,0 +1,24 @@
+export default function Stats({ items }) {
+  console.log(items)
+  if (!items.length) {
+    return (
+      <p className='stats'>
+        <em>Start adding your items to your packing list</em>
+      </p>
+    )
+  }
+
+  const numItems = items.length
+  const numPacked = items.filter((item) => item.packed).length
+  const percentage = Math.round((numPacked / numItems) * 100)
+
+  return (
+    <footer className='stats'>
+      <em>
+        {percentage === 100
+          ? 'You got everything to go ✈️'
+          : `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  )
+}
